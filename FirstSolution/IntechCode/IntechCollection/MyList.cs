0290a7f1303eee875e8634e0dc758ecd7e406891
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-03-15 13:03:44 +0100

    MyList (todo).

--- FirstSolution/IntechCode/IntechCollection/MyList.cs
+++ FirstSolution/IntechCode/IntechCollection/MyList.cs
@@ -2,7 +2,34 @@
 
 namespace IntechCode.IntechCollection
 {
-    public class MyList : IMyList<T>
+    public class MyList<T> : IMyList<T>
     {
+        public T this[int index] 
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public int Count => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
