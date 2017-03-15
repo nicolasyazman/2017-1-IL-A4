ba860be7aa9e41dd330d917a0c3cbb3e0d4f82e2
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-03-15 11:02:12 +0100

    First commit.

---
+++ FirstSolution/IntechCode/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace IntechCode
+{
+    public class HelloWorld
+    {
+        public static string SayHelloWorld( int count )
+        {
+#if DEBUG
+            Console.WriteLine("Hello world!");
+#else
+            Console.WriteLine("Hello world! (in Release!)");
+#endif
+            return $"Count is {count:X}";
+        }
+
+    }
+}
---
+++ FirstSolution/Tests/IntechCode.Tests/HelloWorldTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace IntechCode.Tests
+{
+    public class HelloWorldTests
+    {
+        [Fact]
+        public void HelloWorld_says_hello_in_hexadecimal()
+        {
+            HelloWorld.SayHelloWorld(10).Should().Be("Count is A");
+            (4 + 5).Should().Be(9);
+        }
+    }
+}
