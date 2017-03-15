f2e0a57d0c786041046fdb0d5b951c161ac6f48c
NicolasY <nicolasyazman@gmail.com>
2017-03-15 16:52:59 +0100

    Modifications mylist

--- FirstSolution/IntechCode/IntechCollection/MyList.cs
+++ FirstSolution/IntechCode/IntechCollection/MyList.cs
@@ -4,32 +4,107 @@ namespace IntechCode.IntechCollection
 {
     public class MyList<T> : IMyList<T>
     {
-        public T this[int index] 
+        private T[] _data;
+        private int _count;
+
+        public MyList()
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            _data = new T[10];
+            _count = 0;
         }
 
-        public int Count => throw new NotImplementedException();
+        public T this[int index] 
+        {
+           
+            get
+            {
+                if (index >= Count)
+                {
+                    throw new IndexOutOfRangeException("Index " + index + " is not within acceptable range : [" + 0 + ", " + Count + "[");
+                }
+                return  _data[index];
+            }
+            set
+            {
+                if (index >= Count)
+                {
+                    throw new IndexOutOfRangeException("Index " + index + " is not within acceptable range : [" + 0 + ", " + Count + "[");
+                }
+                _data[index] = value;
+            }
+        }
+
+        public int Count => _count;
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            if (Count == _data.Length) // Array full, must instantiate new, bigger array (size *2)
+            {
+                T[] newData = new T[_data.Length * 2];
+                for (int i = 0; i < _data.Length; i++)
+                    newData[i] = _data[i];
+                newData[_data.Length] = item;
+                _data = newData;
+            }
+            else
+            {
+                _data[Count] = item;
+            }
+            _count++;
         }
 
         public int IndexOf(T item)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < Count; i++)
+            { 
+                if (_data[i].Equals(item))
+                    return i;
+            }
+            return -1;
         }
 
         public void Insert(int index, T item)
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+            {
+                throw new Exception("No item in the array, so cannot remove");
+            }
+            if (index >= Count || index < 0)
+            {
+                throw new IndexOutOfRangeException("Index " + index + " is not within acceptable range : [" + 0 + ", " + Count + "[");
+            }
+
+            if (Count == _data.Length) // Array full, must instantiate new, bigger array (size *2)
+            {
+                T[] newData = new T[_data.Length * 2];
+                for (int i = 0; i < _data.Length; i++)
+                    newData[i] = _data[i];
+                newData[_data.Length] = item;
+                _data = newData;
+            }
+            for (int i = index + 1; i <= Count; i++)
+            {
+                _data[i] = _data[i - 1];
+            }
+            _data[index] = item;
+            _count++;
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+            {
+                throw new Exception("No item in the array, so cannot remove");
+            }
+            if (index >= Count || index < 0)
+            {
+                throw new IndexOutOfRangeException("Index " + index + " is not within acceptable range : [" + 0 + ", " + Count + "[");
+            }
+            for (int i = index; i < Count - 1; i++)
+            {
+                _data[i] = _data[i + 1];
+            }
+            _count--;
         }
     }
 }
---
+++ FirstSolution/Tests/IntechCode.Tests/IntechCollectionTests/IMyListTests.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using IntechCode.IntechCollection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IntechCode.Tests.IntechCollection.Tests
+{
+    [NUnit.Framework.TestFixture]
+    public class MyListTests
+    {
+        [NUnit.Framework.Test]
+        [Fact]
+        public void MyList_instantiated_count_is_zero()
+        {
+            // Arrange
+            MyList<int> mylist = new MyList<int>();
+
+            // Act
+
+            // Assert
+            mylist.Count.Should().Be(0);
+        }
+
+        [NUnit.Framework.Test]
+        [Fact]
+        public void MyListAddTest()
+        {
+            // Arrange
+            MyList<int> mylist = new MyList<int>();
+
+            // Act
+            mylist.Add(3);
+            // Assert
+            mylist.Count.Should().Be(1);
+            mylist[0].Should().Be(3);
+        }
+
+        [NUnit.Framework.Test]
+        [Fact]
+        public void MyListAddBourrin()
+        {
+            // Arrange
+            MyList<int> mylist = new MyList<int>();
+
+            // Act
+            for (int i = 0; i < 1283; i++)
+            {
+                mylist.Add(i);
+            }
+            // Assert
+            mylist.Count.Should().Be(1283);
+            for (int i = 0; i < 1283; i++)
+                mylist[i].Should().Be(i);
+        }
+
+        [NUnit.Framework.Test]
+        [Fact]
+        public void MyListRemoveElement()
+        {
+            // Arrange
+            MyList<int> mylist = new MyList<int>();
+
+            // Act
+            for (int i = 0; i < 10; i++)
+            {
+                mylist.Add(i);
+            }
+            mylist.RemoveAt(5);
+            // Assert
+            mylist.Count.Should().Be(9);
+            for (int i = 0; i < 5; i++)
+                mylist[i].Should().Be(i);
+            for (int i = 5; i < 9; i++)
+                mylist[i].Should().Be(i + 1);
+        }
+    }
+}
