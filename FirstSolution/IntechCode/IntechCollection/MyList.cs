d15fb36cadb88e064c14a61811916da4f811e4f4
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-03-15 17:22:40 +0100

    ToDo implementation for enumerator.

--- FirstSolution/IntechCode/IntechCollection/MyList.cs
+++ FirstSolution/IntechCode/IntechCollection/MyList.cs
@@ -4,36 +4,83 @@ namespace IntechCode.IntechCollection
 {
     public class MyList<T> : IMyList<T>
     {
+        T[] _items;
+        int _count;
+
+        public MyList()
+        {
+            _items = new T[16];
+        }
+
         public T this[int index] 
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            get
+            {
+                if (index >= _count) throw new IndexOutOfRangeException();
+                return _items[index];
+            }
+            set
+            {
+                if (index >= _count) throw new IndexOutOfRangeException();
+                _items[index] = value;
+            }
         }
 
-        public int Count => throw new NotImplementedException();
+        public int Count => _count;
 
-        public void Add(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int IndexOf(T item)
-        {
-            throw new NotImplementedException();
-        }
+        public void Add(T item) => Insert(_count, item);
 
         public void Insert(int index, T item)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index > _count) throw new IndexOutOfRangeException();
+            if (_count == _items.Length)
+            {
+                // Realloc needed.
+                T[] newItems = new T[ _items.Length * 2];
+                Array.Copy(_items, 0, newItems, 0, _count);
+                _items = newItems;
+            }
+            if (index == _count) _items[_count] = item;
+            else
+            {
+                Array.Copy(_items, index, _items, index + 1, _count - index);
+                _items[index] = item;
+            }
+            ++_count;
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index >= _count) throw new IndexOutOfRangeException();
+            Array.Copy(_items, index+1, _items, index, _count - index -1);
+            _items[--_count] = default(T);
         }
+
+        public int IndexOf(T item)
+        {
+            for( int i = 0; i < _count; ++i)
+            {
+                if (System.Collections.Generic.EqualityComparer<T>.Default.Equals(item, _items[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        class E : IMyEnumerator<T>
+        {
+            public T Current => throw new NotImplementedException();
+
+            public bool MoveNext()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
         public IMyEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new E();
         }
 
     }
