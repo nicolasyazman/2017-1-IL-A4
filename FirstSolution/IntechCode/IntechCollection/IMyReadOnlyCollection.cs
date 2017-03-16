eada2770bfde49af7f82797b3a61f6b4d7bf6a9a
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-03-16 11:50:58 +0100

    ReadOnly Collection & List.

---
+++ FirstSolution/IntechCode/IntechCollection/IMyReadOnlyCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntechCode.IntechCollection
+{
+    public interface IMyReadOnlyCollection<out T> : IMyEnumerable<T>
+    {
+        int Count { get; }
+    }
+}
---
+++ FirstSolution/IntechCode/IntechCollection/IMyReadOnlyList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntechCode.IntechCollection
+{
+    public interface IMyReadOnlyList<out T> : IMyReadOnlyCollection<T>
+    {
+        T this[int index] { get; }
+
+    }
+}
